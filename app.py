c6bf1c3d1cfdc40383a6cc1a8a22272e1dc5d9e8
MaximusB93 <107109667+MaximusB93@users.noreply.github.com>
2025-03-19 21:54:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-# Замените на URL вашего Google Apps Script
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzTguj7UsMoXgNLfmWp18x4sP7Pl7rxiM_T399hlo-iIWExThVSGzQeHOn3MG1iwxKdig/exec"
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    phone = data['contact']['phone']
-    message = data['message']['body']
-
-    params = {
-        'phone': phone,
-        'message': message
-    }
-
-    # Отправляем данные в Google Таблицы
-    requests.get(GOOGLE_SCRIPT_URL, params=params)
-
+    print("Полученные данные:", data)
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=10000)
